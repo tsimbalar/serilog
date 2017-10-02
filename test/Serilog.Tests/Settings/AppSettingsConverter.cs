5868a041f0948f70ac44d27d94b6ec080183f7dd
Thibaud Desodt <tibo.desodt@gmail.com>
2017-10-02 17:12:15 +0200

    Generate directives for minimum-level

---
+++ test/Serilog.Tests/Settings/AppSettingsConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Serilog.Events;
+
+namespace Serilog.Tests.Settings
+{
+    
+
+    public static class AppSettingsConverter
+    {
+        public static IEnumerable<KeyValuePair<string, string>> From(Expression<Func<LoggerConfiguration, LoggerConfiguration>> exp)
+        {
+            return FromRightToLeft(exp).Reverse();
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> FromRightToLeft(Expression<Func<LoggerConfiguration, LoggerConfiguration>> exp)
+        {
+            if (exp == null) throw new ArgumentNullException(nameof(exp));
+
+            var current = exp.Body;
+
+            while (current is MethodCallExpression)
+            {
+                var methodCall = (MethodCallExpression)current;
+                var leftSide = (MemberExpression)((MethodCallExpression)current).Object;
+                if (leftSide.Member.DeclaringType == typeof(LoggerConfiguration))
+                {
+                    if (leftSide.Member.Name == nameof(LoggerConfiguration.MinimumLevel))
+                    {
+                        if (Enum.TryParse(methodCall.Method.Name, out LogEventLevel minimumLevel))
+                        {
+                            yield return new KeyValuePair<string, string>("minimum-level", minimumLevel.ToString());
+                        }
+                    }
+                }
+
+                current = leftSide.Expression;
+            }
+
+            //var instanceMethodCall = (MethodCallExpression)exp.Body;
+            //var objectExpression = (MemberExpression)instanceMethodCall.Object;
+
+
+
+            //if (objectExpression.Member.DeclaringType == typeof(LoggerConfiguration) && objectExpression.Member.Name == nameof(LoggerConfiguration.MinimumLevel))
+            //{
+            //    LogEventLevel minimumLevel;
+            //    if (Enum.TryParse(instanceMethodCall.Method.Name, out minimumLevel))
+            //    {
+            //        yield return new KeyValuePair<string, string>("minimum-level", minimumLevel.ToString());
+            //    }
+
+            //}
+
+            //if (objectExpression.Expression is MethodCallExpression)
+            //{
+            //    instanceMethodCall = (MethodCallExpression) objectExpression.Expression;
+            //    objectExpression = (MemberExpression) instanceMethodCall.Object;
+
+            //    if (objectExpression.Member.DeclaringType == typeof(LoggerConfiguration) && objectExpression.Member.Name == nameof(LoggerConfiguration.MinimumLevel))
+            //    {
+            //        LogEventLevel minimumLevel;
+            //        if (Enum.TryParse(instanceMethodCall.Method.Name, out minimumLevel))
+            //        {
+            //            yield return new KeyValuePair<string, string>("minimum-level", minimumLevel.ToString());
+            //        }
+
+            //    }
+            //}
+        }
+
+    }
+}
---
+++ test/Serilog.Tests/Settings/AppSettingsFromExpressionTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Serilog.Tests.Settings
+{
+    public class AppSettingsFromExpressionTests
+    {
+        [Fact]
+        public void SupportMinimumLevel()
+        {
+            var actual = AppSettingsConverter.From(lc =>
+                lc
+                    .MinimumLevel.Verbose()
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Information()
+                    .MinimumLevel.Warning()
+                    .MinimumLevel.Error()
+                    .MinimumLevel.Fatal()
+            ).ToList();
+
+            var expected = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("minimum-level", "Verbose"),
+                new KeyValuePair<string, string>("minimum-level", "Debug"),
+                new KeyValuePair<string, string>("minimum-level", "Information"),
+                new KeyValuePair<string, string>("minimum-level", "Warning"),
+                new KeyValuePair<string, string>("minimum-level", "Error"),
+                new KeyValuePair<string, string>("minimum-level", "Fatal")
+            };
+
+            Assert.Equal(expected.ToList(), actual, new KeyValuePairComparer<string, string>());
+        }
+
+        public class KeyValuePairComparer<TK, TValue> : IEqualityComparer<KeyValuePair<TK, TValue>>
+        {
+            public bool Equals(KeyValuePair<TK, TValue> x, KeyValuePair<TK, TValue> y)
+            {
+                return x.Key.Equals(y.Key) && x.Value.Equals(y.Value);
+            }
+
+            public int GetHashCode(KeyValuePair<TK, TValue> obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+    }
+}
